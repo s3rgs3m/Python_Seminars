3707bd974352668180439d083b7d91874c09edd3
s3rgs3m <sergsem@gmail.com>
2023-05-25 21:08:00 +0300

    Task36

--- Seminar7/task36.py
+++ Seminar7/task36.py
@@ -15,3 +15,12 @@
 5 10 15 20 25 30
 6 12 18 24 30 36 
 '''
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for i in range(1, num_rows+1):
+        strArr = []
+        for j in range(1, num_columns+1):
+            strArr.append(operation(i,j))
+        print (strArr)
+
+print_operation_table(lambda x, y: x * y)
